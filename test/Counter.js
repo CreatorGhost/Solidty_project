39571dc23c8c01fd11bfb1f8ac7e82152d71ab90
Aditya Pratap Singh <cybercreed010@gmail.com>
2023-02-07 12:25:14 +0530

    made changes to test cases

--- test/Counter.js
+++ test/Counter.js
@@ -2,16 +2,34 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
-describe("Counter", ()=>
-    {
-        let counter;
 
-        it("Testing count",async()=>
-        {
-            const Counter = await ethers.getContractFactory('Counter');
-            counter = await Counter.deploy("Aditya",7);
-            count = await counter.count();
-            expect (count).to.equal(7);
-        })
-    }   
-)
+
+describe ("Counter",()=>{
+    let counter;
+
+    beforeEach(async()=>{
+            //setting connection with ether to our smart contact
+            const Counter =await ethers.getContractFactory("Counter")
+            // Deploying our smart contract 
+            counter = await Counter.deploy("Adi",23);
+    })
+
+    it("Cheking for count",async()=>{
+        
+        count =  await counter.count();
+        expect (count).equal(23)
+    })
+    it("Checking for name",async ()=>{
+        //const Counter = await ethers.getContractFactory("Counter")
+        _name = await counter.name();
+        expect (_name).equal("Adi")
+    })
+})
+
+
+
+
+
+
+
+
